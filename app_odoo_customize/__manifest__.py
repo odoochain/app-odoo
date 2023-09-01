dfaad2002378c2ea20c4d818495cfa086bb930e3
odoochain <75811889+odoochain@users.noreply.github.com>
2023-09-02 00:07:22 +0800

    update

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -1,30 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Created on 2018-11-26
-# author: 欧度智能，https://www.odoochain.cn
-# email: 300883@qq.com
-# resource of odoochain
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-# Odoo12在线用户手册（长期更新）
-# https://www.odoochain.cn/documentation/user/12.0/en/index.html
-
-# Odoo12在线开发者手册（长期更新）
-# https://www.odoochain.cn/documentation/12.0/index.html
-
-# Odoo10在线中文用户手册（长期更新）
-# https://www.odoochain.cn/documentation/user/10.0/zh_CN/index.html
-
-# Odoo10离线中文用户手册下载
-# https://www.odoochain.cn/odoo10_user_manual_document_offline/
-# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
-# https://www.odoochain.cn/odoo10_developer_document_offline/
-# description:
-
 {
     'name': 'odoo Tweak,Boost,Customize,Ai All in One. Customize,UI,Boost,Security,Data,Development Enhance',
-    'version': '16.23.08.24',
-    'author': 'odoochain.cn',
+    'version': '16.23.08.28',
+    'author': 'odoochain,odooai',
     'category': 'Extra Tools',
     'website': 'https://www.odoochain.cn',
     'live_test_url': 'https://demo.odooapp.cn',
