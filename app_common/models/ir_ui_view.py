6ce204a4c89c7b66f199e7b7946ae41551c01375
odoochain <75811889+odoochain@users.noreply.github.com>
2023-11-03 03:48:19 +0800

    todo fix common

--- app_common/models/ir_ui_view.py
+++ app_common/models/ir_ui_view.py
@@ -27,7 +27,7 @@ def app_relaxng(view_type):
                 _relaxng_cache[view_type] = None
     return _relaxng_cache[view_type]
 
-view_validation.relaxng = app_relaxng
+# view_validation.relaxng = app_relaxng
 
 class View(models.Model):
     _inherit = 'ir.ui.view'
@@ -35,7 +35,7 @@ class View(models.Model):
     def __init__(self, env, ids, prefetch_ids):
         # 这里应该是无必要，但为了更安全
         super(View, self).__init__(env, ids, prefetch_ids)
-        view_validation.relaxng = app_relaxng
+        # view_validation.relaxng = app_relaxng
 
     # todo: 有可能需要处理增加的 header等标签
     # 直接重写原生方法
